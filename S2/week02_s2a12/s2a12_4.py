7e5bfdc5d53d1d1d88873d1180ef64f2c66ad873
Kingsley1116 <93391504+Kingsley1116@users.noreply.github.com>
2023-09-05 22:09:21 +0800

    Update s2a12_4.py

--- S2/week02_s2a12/s2a12_4.py
+++ S2/week02_s2a12/s2a12_4.py
@@ -4,26 +4,22 @@ print("s2a12 李景霖")
 
 print("請猜1-100中的隨機的年齡是多少，你共有5次機會! \n")
 
-try:
-    randomNumber: int = random.randint(1, 100)
+randomNumber = random.randint(1, 100)
 
-    i = 1
-    while i <= 5:
-        guess = int(input("請輸入你猜測的年齡: "))
+i = 1
+while i <= 5:
+    guess = int(input("請輸入你猜測的年齡: "))
 
-        if guess == randomNumber:
-            print("你猜對了!")
-            break
+    if guess == randomNumber:
+        print("你猜對了!")
+        break
 
-        elif guess < randomNumber:
-            print("你猜小了!")
+    elif guess < randomNumber:
+        print("你猜小了!")
 
-        elif guess > randomNumber:
-            print("你猜大了!")
+    elif guess > randomNumber:
+        print("你猜大了!")
         
-        i += 1
+    i += 1
     
-    print(f"隨機的年齡為: {randomNumber}")
-
-except ValueError:
-    print("請輸入數字!")
+print(f"隨機的年齡為: {randomNumber}")
