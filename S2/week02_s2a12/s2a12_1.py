87e6d71349107381bbb4a7e53418bca1ff3b3b7f
Kingsley1116 <93391504+Kingsley1116@users.noreply.github.com>
2023-09-05 22:08:36 +0800

    Update s2a12_1.py

--- S2/week02_s2a12/s2a12_1.py
+++ S2/week02_s2a12/s2a12_1.py
@@ -2,14 +2,10 @@ print("s2a12 李景霖")
 
 print("計算梯形的面積(公式: 上底加下底乘高除二)\n")
 
-try:
-    upperb = int(input("輸入梯形中上底(upperb)的長度(cm): "))
-    underb = int(input("輸入梯形中下底(underb)的長度(cm): "))
-    height = int(input("輸入梯形中高(height)的長度(cm): "))
+upperb = int(input("輸入梯形中上底(upperb)的長度(cm): "))
+underb = int(input("輸入梯形中下底(underb)的長度(cm): "))
+height = int(input("輸入梯形中高(height)的長度(cm): "))
 
-    trapezoid = ((upperb + underb) * height) / 2
-
-    print(f"梯形的面積是: {trapezoid}")
-
-except ValueError:
-    print("請輸入數字!")
+trapezoid = ((upperb + underb) * height) / 2
+    
+print(f"梯形的面積是: {trapezoid}")
